fa518966e851a066acb03abb77f4e4af3457e8b9
sour-soup <kiziyepi8@gmail.com>
2024-11-18 00:17:58 +0400

    refactor: remove unnecessary logging from NginxLogParser

--- src/main/java/backend/academy/log/analyzer/parser/NginxLogParser.java
+++ src/main/java/backend/academy/log/analyzer/parser/NginxLogParser.java
@@ -5,10 +5,8 @@ import backend.academy.log.analyzer.model.LogRecord;
 import java.time.OffsetDateTime;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import lombok.extern.log4j.Log4j2;
 import static backend.academy.log.analyzer.utils.LogDateTimeFormatter.LOG_DATE_TIME_FORMATTER;
 
-@Log4j2
 public class NginxLogParser implements LogParser {
     private static final Pattern LOG_PATTERN = Pattern.compile("""
         ^(?<remoteAddress>\\S+) - (?<remoteUser>\\S+) \\[(?<dateTime>.+?)] "(?<request>.+?)" \
@@ -18,9 +16,7 @@ public class NginxLogParser implements LogParser {
         Matcher matcher = LOG_PATTERN.matcher(logLine);
 
         if (!matcher.matches()) {
-            String message = "Invalid log line format: " + logLine;
-            log.error(message);
-            throw new LogParserException(message);
+            throw new LogParserException("Invalid log line format: " + logLine);
         }
 
         String remoteAddress = matcher.group("remoteAddress");
